588f2ab31d83022e4ff25a839770481b42de9f9f
wuming <1191552161@qq.com>
2018-01-16 13:42:19 +0800

    修改：     crontab.sh 	修改：     frontend/web/price.json

--- crontab.sh
+++ crontab.sh
@@ -1,10 +1,8 @@
-﻿#!/bin/bash  
-      
-step=1  
-      
-for (( i = 0; i < 60; i=(i+step) )); do  
-    $(php '/www/web/100gift/public_html/yii' 'init/gather2')
-     sleep $step  
-done  
-      
-exit 0  
+﻿#!/bin/bash
+step=1
+for (( i = 0; i < 60; i=(i+step) )); do
+    # $(php '/www/web/100gift/public_html/yii' 'init/gather2')
+    $(php yii 'init/gather2')
+     sleep $step
+done
+exit 0
