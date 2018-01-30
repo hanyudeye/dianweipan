457d3331761b6145d01d4559ba09051d90cba5af
wuming <1191552161@qq.com>
2018-01-30 08:52:22 +0800

    新文件：   frontend/views/user/zfbpay.php

---
+++ frontend/views/user/zfbpay.php
@@ -0,0 +1,12 @@
+<!-- <link rel="stylesheet" type="text/css" href="/css/login.css" /> -->
+<?php common\components\View::regCss('iconfont/iconfont.css') ?>
+<style>
+ img { height: auto; width: auto\9; width:100%; }
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 text-center" >
+            <img style="margin: 0px auto; display: block;" src="<?= $img ?>"> 
+        </div>
+    </div>
+</div>
